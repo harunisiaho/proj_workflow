5869055eecae126d1496a05312ae017541afde6a
Harun Isiaho <harunisiaho@gmail.com>
2020-10-04 22:01:56 +0000

    Changed char pointer to const char pointer

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 include <stdio.h>
 
-size_t my_strlen(char *s)
+size_t my_strlen(const char *s)
 {
    char *p = s;
 
@@ -13,14 +13,14 @@ size_t my_strlen(char *s)
 int main(void)
 {
    int i;
-   char *s[] = 
+   char *s[] =
    {
       "Git tutorials",
       "Tutorials Point"
    };
 
    for (i = 0; i < 2; ++i)
-      
+
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
 
    return 0;
